a884d46348e3379e09a7c36b59cbe1691b9c94f4
PhilipKlaus <philip.klaus@ait.ac.at>
2021-06-07 16:49:49 +0200

    Remove unneeded source files

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,1 @@
-import { PlyAscii } from "./plyAscii";
-import { extractHeader } from "./tools";
-
-function loadFromString(src: string) {
-  let header = extractHeader(src);
-  let body = src.slice(header.length + 1);
-  return new PlyAscii(header, body);
-}
-
-export { loadFromString, PlyAscii };
+export {};
